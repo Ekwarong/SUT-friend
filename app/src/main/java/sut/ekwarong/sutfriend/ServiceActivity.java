6398a190da5ed8940225c84733c691a9ec5e0a68
masterung <phrombutr@gmail.com>
2016-09-23 15:09:20 +0700

    Design UI Detail

--- app/src/main/java/sut/ekwarong/sutfriend/ServiceActivity.java
+++ app/src/main/java/sut/ekwarong/sutfriend/ServiceActivity.java
@@ -1,7 +1,10 @@
 package sut.ekwarong.sutfriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -37,6 +40,21 @@ public class ServiceActivity extends AppCompatActivity {
         // Show ListView
         MyAdapter myAdapter = new MyAdapter(this, nameStrings, genderStrings, imageStrings);
         listView.setAdapter(myAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+
+                Intent intent = new Intent(ServiceActivity.this, DetailActivity.class);
+                intent.putExtra("Image", imageStrings[i]);
+                intent.putExtra("Name", nameStrings[i]);
+                intent.putExtra("Gender", genderStrings[i]);
+                intent.putExtra("Address", addressStrings[i]);
+                intent.putExtra("Phone", phoneStrings[i]);
+                startActivity(intent);
+
+            }   // OnItemClick
+        });
 
     }   // Main Method
 }   // Main Class
